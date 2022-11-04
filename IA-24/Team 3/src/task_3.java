0f6eccc7743ff5c669d9d6663f6e7062aa9c7dd9
anchous <117158719+annaorlovskaaa@users.noreply.github.com>
2022-11-04 02:00:23 +0200

    Update task_3.java

--- IA-24/Team 3/src/task_3.java
+++ IA-24/Team 3/src/task_3.java
@@ -2,33 +2,28 @@ package op.lab;
 
 public class task_3 {
 
-
     public static void main(String[] args) {
         int[] a = {6, 1, -9, -3, 8, 7};
         printResults(a);
 
     }
-
     static int result(int[] a) {
         int max = a[0];
         for (int i = 1; i < a.length; i++) {
-            if (i == Double.NaN) {
+            if (Double.isNaN(i)) {
                 throw new IllegalArgumentException("Недопустимий елемент = " + i);
             }
             max = Math.max(max, a[i]);
         }
-
         return max;
     }
-    private static double printResults(int[] a) {
+    private static void printResults(int[] a) {
         System.out.print("Максимальний елемент = " );
         try {
             System.out.println(result(a));
         } catch (IllegalArgumentException e) {
             System.out.println("Увага! " + e.getMessage());
         }
-
-        return 0;
     }
 }
 
