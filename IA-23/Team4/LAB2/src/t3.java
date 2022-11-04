8b03a1292619d3e1430e2155fdfaf97ee7dd5abe
sofiakashubiak <116309310+sofiakashubiak@users.noreply.github.com>
2022-11-05 01:27:14 +0200

    Lab_2

---
+++ IA-23/Team4/LAB2/src/t3.java
@@ -0,0 +1,20 @@
+import static java.lang.Math.*;
+
+public class t3 {
+
+    public static void main(String[] args) {
+
+        double a=1.23;
+        double b=-0.34;
+        double c=0.707;
+        double d=2.312;
+
+        double y;
+
+        y=pow((4*(sinh(sqrt(abs(a/b)))))+(3*asin(c)),d);
+
+        System.out.println("y="+y);
+
+    }
+
+}
