917db840c94c871c0d17e018aa2ecc33472ce4cd
jlmp3mp44 <julya.meleshko@gmail.com>
2022-11-08 11:59:08 +0200

    Lab_5:Add tasks my group and a report

---
+++ IA-24/Team 5/LAB5/src/task5.java
@@ -0,0 +1,68 @@
+package lab.five;
+public class task5 {
+}
+    class matrix {
+        public static void main(String args[]) {
+            double[][] matrix1 = {
+                    {1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, 1, 2, 3},
+                    {7, 3, 2, 1}
+            };
+            double[][] matrix2 = {
+                    {1, 2, 3, 4, 8, 9},
+                    {5, 6, 7, 8, 9, 1},
+                    {9, 1, 2, 3, 6, 2},
+                    {7, 3, 2, 1, 3, 1},
+                    {2, 1, 6, 5, 3, 4},
+                    {6, 3, 2, 1, 5, 4},
+            };
+            double[][] matrix3 = {
+                    {1, 2, Double.MAX_VALUE, 4},
+                    {5, 6, 7, 8},
+                    {9, 1, 2, 3},
+                    {7, 3, 2, 1}
+            };
+            double[][] matrix4 = {
+                    {1, 2, 3, 4},
+                    {5, 6, 7, 8},
+                    {9, Double.NaN, 2, 3},
+                    {7, 3, 2, 1}
+            };
+            printResults(matrix1);
+            printResults(matrix2);
+            printResults(matrix3);
+            printResults(matrix4);
+        }
+
+        static String clearNE(double[][] matrix) {
+            System.out.println("New matrix:");
+            int len = matrix.length;
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < len; j++) {
+                    if (matrix[i][j] == Double.MAX_VALUE || Double.isNaN(matrix[i][j])) {
+                        throw new IllegalArgumentException();
+                    }
+                    if (i > j) matrix[i][j] = (int) matrix[i][j];
+                    else {
+                        matrix[i][j] = (int) 0;
+                    }
+                    System.out.print((int) matrix[i][j] + "  ");
+                }
+                System.out.println();
+            }
+            return (" ");
+        }
+
+        public static void printResults(double[][] matrix) {
+            try {
+                System.out.println(clearNE(matrix));
+            } catch (IllegalArgumentException e) {
+                System.out.println("Exception!!! Change the element of matrix");
+            }
+        }
+    }
+
+
+
+
