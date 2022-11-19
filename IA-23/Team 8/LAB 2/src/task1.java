f50b20e75d52fee249973e0332a63f748f03554c
kova1skii <kovalskiidrug@gmail.com>
2022-11-19 01:09:33 +0100

    Delete task1.java

--- IA-23/Team 8/LAB 2/src/task1.java
+++
@@ -1,8 +0,0 @@
-public class task1 {
-    public static void main(String[] args){
-        double a=0.345, b= -2.25, c=2.65, d=3.99, y;
-        y=(5*a)/Math.sin(a)+Math.sqrt(Math.tanh(Math.abs(b)*c)/Math.log(d));
-        System.out.print(y);
-    }
-
-}
