58339ce377543ed3cf0bdbb6b9e27052e7895559
kova1skii <kovalskiidrug@gmail.com>
2022-11-19 01:09:38 +0100

    Delete task3.java

--- IA-23/Team 8/LAB 2/src/task3.java
+++
@@ -1,8 +0,0 @@
-public class task3 {
-    public static void main(String[] args){
-        double a=1.27, b=10.99, c=4, d=-25.32, y;
-        y=Math.pow(Math.tanh(a),1/c)/2-(Math.cosh(b)/Math.log(Math.abs(d+c)));
-        System.out.print(y);
-
-    }
-}
