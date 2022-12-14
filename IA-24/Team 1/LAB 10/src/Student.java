4856ebf952d8740ddeff0b5529a5075dc7c0c982
prtrymer <114065579+prtrymer@users.noreply.github.com>
2022-12-14 20:32:27 +0200

    Add files via upload

---
+++ IA-24/Team 1/LAB 10/src/Student.java
@@ -0,0 +1,12 @@
+public class Student extends Faculty {
+    String name;
+    String surname;
+    int number;
+    double avg_mark;
+    public Student(String name,String surname,int number,double avg_mark){
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+        this.avg_mark = avg_mark;
+    }
+}
