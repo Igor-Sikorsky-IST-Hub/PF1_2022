18386b215a7ed7319b8cbf9a2c4601383ffba592
dLiadsk <dimaluad2@gmail.com>
2022-12-14 19:42:24 +0200

    LAB_9: updated code

--- IA-23/Team 5/LAB 9/scr/task11.java
+++ IA-23/Team 5/LAB 9/scr/task11.java
@@ -1,21 +1,19 @@
+import java.util.regex.Pattern;
+
 public class task11 {
     public static void main(String[] args){
         printResult("fOr Exampl3 wE USE this Method");
         printResult("W54e ALSO CAN US35645 THIS EX548MPLE F0541R THIS M3597THOD");
         printResult("and457 also57412 a11 l0wer case letters");
         printResult("575217325341132354261371923712");
-        printResult("8745484621349543906734");
+        printResult("8745484621349 543906734");
+        printResult("");
+
     }
     public static String upperToLower(String text){
-
-        boolean isOnlyDigits = true;
-        for(int i = 0; i < text.length() && isOnlyDigits; i++) {
-            if(!Character.isDigit(text.charAt(i))) {
-                isOnlyDigits = false;
-            }
-        }
-        if (isOnlyDigits){
-            throw new IllegalArgumentException("text its only numbers :(");
+        boolean verify = text.matches("[0-9]+[\\. ]?[0-9]*");
+        if (verify | text.isEmpty()) {
+            throw new IllegalArgumentException("This text consist only numbers or Empty");
         } else {
             return text.toLowerCase();
         }
--- IA-23/Team 5/LAB 9/scr/task5.java
+++ IA-23/Team 5/LAB 9/scr/task5.java
@@ -3,28 +3,18 @@ public class task5 {
         printResult("01111");
         printResult("10111");
         printResult("01111111");
+        printResult("");
         printResult("example");
-        printResult("another example");
     }
 
     public static int binaryStringToInt(String numbersText){
-        boolean isOnlyDigits = true;
-        for(int i = 0; i < numbersText.length() && isOnlyDigits; i++) {
-            if(!Character.isDigit(numbersText.charAt(i))) {
-                isOnlyDigits = false;
-            }
-        }
-        if (!isOnlyDigits){
-            throw new IllegalArgumentException("text of numbers not consist numbers :(");
-        } else {
-            return Integer.parseInt(numbersText,2);
-        }
+        return Integer.parseInt(numbersText, 2);
     }
     public static void printResult(String text){
         try {
             System.out.println(binaryStringToInt(text));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Expection!!! " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Expection!!! where numbers? ");
         }
     }
 }
