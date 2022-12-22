682e5862f298e6cdbffea31664076ca9f5493bd4
unknown <orlovska.anna@LLL.kpi.ua>
2022-12-22 19:15:43 +0200

    Lab_10: added task created by Roma Krasnoshapka and Anna Orlovska

---
+++ IA-24/Team 3/LAB 10/src/Film.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Film{
+    private String name;
+    private List<Actor> actors = new ArrayList();
+    public Film(String name) {
+        this.name = name;
+    }
+    public void addActor(Actor actor) {
+        this.actors.add(actor);
+    }
+    public List<Actor> getActors(){
+        return this.actors;
+    }
+    public String getName() {
+        return this.name;
+    }
+}
