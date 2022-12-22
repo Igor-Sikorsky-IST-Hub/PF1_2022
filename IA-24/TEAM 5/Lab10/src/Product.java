d3c9e9318158c05fe9f1f6f005a544b7a2c2117e
Hrystynkkaa <hristinaganza3@gmail.com>
2022-12-22 16:04:16 +0200

    Lab_10

---
+++ IA-24/TEAM 5/Lab10/src/Product.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Product {
+    private int price;
+    private String product;
+
+    public Product(String product, int price) {
+        this.product = product;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return product + " price: "+ price;
+    }
+}
