c6642dca5ed3da8ff52636013cdc46f7b22dd7f9
unknown <orlovska.anna@LLL.kpi.ua>
2022-12-22 18:55:09 +0200

    Lab_10: added task created by Roma Krasnoshapka and Anna Orlovska

--- IA-24/Team 3/LAB10/src/Main.java
+++ IA-24/Team 3/LAB10/src/Main.java
@@ -1,13 +1,17 @@
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+        Database();
+        printResult();
+
+    }
 
     public static void Database() {
-        Film film1 = new Film ("Deadpool");
-        Film film2 = new Film ("Focus");
-        Film film3 = new Film ("The Pursuit of Happyness");
-
+        Film film1 = new Film("Deadpool");
+        Film film2 = new Film("Focus");
+        Film film3 = new Film("The Pursuit of Happyness");
         Actor actor1 = new Actor("Ryan Rodney");
         Actor actor2 = new Actor("Will Smith");
         Actor actor3 = new Actor("Syre Smith");
@@ -31,25 +35,39 @@ public class Main {
         actor3.addFilm(film3);
 
     }
-    public static void main(String[] args){
-        Database();
+
+    private static String mostAcrtor() {
+        int countActor = 0;
+        String mostActored = "";
+        List<Film> allfilms = Database.getFilms();
+        for (Film film : allfilms) {
+            if (countActor < film.getActors().size()) {
+                countActor = film.getActors().size();
+                mostActored = film.getName();
+            } else if (countActor == film.getActors().size() && film.getActors().size() != 0) {
+                mostActored = film.getName() + " and " + mostActored;
+            }
+        }
+        return mostActored;
+    }
+
+    private static void printResult() {
+        System.out.println("The largest number of actors in the film - " + mostAcrtor());
         notFilmed();
     }
 
     private static void notFilmed() {
         List<Actor> allactors = Database.getActors();
 
-        for (Iterator <Actor> i = allactors.iterator(); i.hasNext();) {
+        for (Iterator <Actor> i = allactors.iterator(); i.hasNext(); ) {
             Actor actor = (Actor) i.next();
             if (actor.getFilms().isEmpty()) {
-                System.out.println(actor.getName() +" - didn't act in any film ");
+                System.out.println(actor.getName() + " - didn't act in any film ");
             }
 
+
         }
-
     }
-
-
 }
 
 
