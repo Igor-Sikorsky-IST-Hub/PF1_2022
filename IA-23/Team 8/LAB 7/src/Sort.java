826d72ec3c76aadc43eb3314dcc05cb41ac5e215
kova1skii <kovalskiidrug@gmail.com>
2023-01-09 17:40:04 +0100

    Create Sort.java

---
+++ IA-23/Team 8/LAB 7/src/Sort.java
@@ -0,0 +1,14 @@
+public class Sort {
+    Sorting typeOfSorting;
+    long[] array;
+    public void setTypeOfSorting(Sorting typeOfSorting) {
+        this.typeOfSorting = typeOfSorting;
+    }
+    public void setArray(long[] array) {
+        this.array = array;
+    }
+    public void executeSorting() {
+        long[] result = typeOfSorting.sortSomething(array);
+        typeOfSorting.printResult(result);
+    }
+}
