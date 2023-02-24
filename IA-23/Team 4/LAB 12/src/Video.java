7f43784484bcb4f2bd2af75449efaf735f02e960
Yermak Diana <d.yermak.ia23@kpi.ua>
2023-02-24 21:43:14 +0200

    Lab_12: Added full code and report

---
+++ IA-23/Team 4/LAB 12/src/Video.java
@@ -0,0 +1,58 @@
+package lab12;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Video {
+    String name;
+    String URL;
+    int vidviews;
+    int vidlikes;
+    int viddislikes;
+    static Set<Comment> comments = new HashSet<>();
+
+
+    public Video(String name, String URL, int vidviews, int vidlikes, int viddislikes) {
+        this.name = name;
+        this.URL = URL;
+        this.vidviews = vidviews;
+        this.vidlikes = vidlikes;
+        this.viddislikes = viddislikes;
+
+    }
+
+    public int getVidviews() {
+        return this.vidviews;
+    }
+
+    public int getVidlikes() {
+        return this.vidlikes;
+    }
+
+    public int getViddislikes() {
+        return this.viddislikes;
+    }
+
+
+    public static void addСomment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        lab12.Video video = (lab12.Video) o;
+        return Objects.equals(name, video.name) && URL.equals(video.URL);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, URL);
+    }
+}
