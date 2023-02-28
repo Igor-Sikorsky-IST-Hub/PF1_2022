e7de1693a5f3dbd8841886be3c8e82ebc24a1b0e
Maxim-Khokhol <khokhol.maxim@lll.kpi.ua>
2023-02-28 03:17:19 +0200

    Lab_12: Add full code and report

---
+++ IA-23/Team 1/LAB 12/src/Main.java
@@ -0,0 +1,98 @@
+import java.util.*;
+import java.util.Arrays;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Institute inst = new Institute();
+
+
+
+        HashMap<String, String> fict = new HashMap<>();
+        HashMap<String, String> fbmi = new HashMap<>();
+        HashMap<String, String> fsp = new HashMap<>();
+
+
+
+        Student student1 = new Student("Andrii", "Tiuska", "11111_24", 80.3);
+        Student student2 = new Student("Maxim", "Khokhol", "11111_23", 98.7);
+        Student student3 = new Student("David", "Pozhar", "22222_25", 96.5);
+        Student student4 = new Student("Yevhenii", "Voroniuk", "22222_4", 95);
+        Student student5 = new Student("Vadim", "Voloshyn", "22222_23", 100);
+        Student student6 = new Student("Serhii", "Kalyna", "33333_2", 87);
+        Student student11 = new Student("Vova", "Ragnarok", "22222_4", 5);
+
+
+
+
+        inst.addStudents(Arrays.asList(student1, student2, student3, student4, student5, student6, student11));
+        putStudents(inst, fict, "fict");
+        putStudents(inst, fbmi, "fbmi");
+        putStudents(inst, fsp, "fsp");
+        System.out.println("Students with an average mark of 95-100:");
+        task3(inst);
+
+        System.out.print("Total amount of all students in institute: ");
+        System.out.println(task1(inst));
+        System.out.print("The largest faculty: ");
+        System.out.println(whatIsTheLargestFaculty(fict, fbmi, fsp));
+
+    }
+
+
+    public static void putStudents(Institute institute, HashMap<String, String> faculty, String name) {
+
+        Set<Object> keyAndValue = new HashSet<>();
+        for (Student student : institute.students) {
+            String key = student.id;
+            String value = student.name + " " + student.surname;
+            keyAndValue.add(key.split("_")[0]);
+            if (keyAndValue.contains("11111") && name.equals("fbmi")) faculty.put(key, value);
+            if (keyAndValue.contains("22222") && name.equals("fict")) faculty.put(key, value);
+            if (keyAndValue.contains("33333") && name.equals("fsp")) faculty.put(key, value);
+            keyAndValue.clear();
+        }
+    }
+
+
+    public static String whatIsTheLargestFaculty(HashMap<String, String> fict, HashMap<String, String> fbmi, HashMap<String, String> fsp) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        Faculty facultyFict = new Faculty("22222", "FICT");
+        Faculty facultyFbmi = new Faculty("11111", "FBMI");
+        Faculty facultyFsp = new Faculty("33333", "FSP");
+        int countOfStudentsInFict = fict.size();
+        int countOfStudentsInFbmi = fbmi.size();
+        int countOfStudentsInFsp = fsp.size();
+        treeSet.add(countOfStudentsInFict);
+        treeSet.add(countOfStudentsInFbmi);
+        treeSet.add(countOfStudentsInFsp);
+        if (treeSet.last() == countOfStudentsInFict) return facultyFict + ". Count Of Students: " + fict.size();
+        if (treeSet.last() == countOfStudentsInFbmi) return facultyFbmi + ". Count Of Students: " + fict.size();
+        if (treeSet.last() == countOfStudentsInFsp) return facultyFsp + ". Count Of Students: " + fict.size();
+        return "The largest faculty does not exist";
+    }
+    public static void task3(Institute institute) {
+        for (Student x : institute.students) {
+            if (x.averageMark >= 95) {
+                System.out.print(x.name + " " + x.surname);
+                System.out.println(" ");
+            }
+        }
+    }
+
+    public static int task1(Institute institute) {
+        int count = 0;
+        Iterator iterator = institute.students.iterator();
+
+        while (iterator.hasNext()) {
+            count++;
+            iterator.next();
+        }
+
+        return count;
+    }
+
+}
